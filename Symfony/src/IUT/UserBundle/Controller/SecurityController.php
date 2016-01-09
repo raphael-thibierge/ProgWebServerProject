770a4c4bf0daa7108c589f63c127d9a58d496e91
Raphael Thibierge <raph.t.id@gmail.com>
2016-01-09 14:57:20 +0100

    add signIn page, works well ! redirect to home page

--- Symfony/src/IUT/UserBundle/Controller/SecurityController.php
+++ Symfony/src/IUT/UserBundle/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace IUT\UserBundle\Controller;
 
+use IUT\CatalogBundle\Entity\Abonne;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContextInterface;
@@ -35,9 +36,62 @@ class SecurityController extends Controller
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
 
 
+        $user = new Abonne();
+        $coucou = null;
+        $form = $this->get('form.factory')->createBuilder('form', $user)
+            ->add('login', 'text')
+            ->add('password', 'password')
+            ->add('submit', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isValid()){
+            $coucou ='coucou';
+        }
+
+
+
         return $this->render('IUTUserBundle::login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
+            'form'          => $form->createView(),
+            'user'          => $user,
+            'coucou'           => $coucou,
         ));
     }
+
+    public function signInAction(Request $request){
+        $abonne = new Abonne();
+
+
+        $form = $form = $this->get('form.factory')->createBuilder('form', $abonne)
+            ->add('prenomAbonne', 'text')
+            ->add('nomAbonne', 'text')
+            ->add('login', 'text')
+            ->add('password', 'password')
+            ->add('submit', 'submit')
+            ->getForm();
+
+
+        $form->handleRequest($request);
+        if($form->isValid())
+        {
+            // Mise à jour des données
+            $this->getDoctrine()->getManager()->persist($abonne);
+            $this->getDoctrine()->getManager()->flush();
+            // Renvoi vers une autre url
+            $router = $this->container->get('router');
+            $url = $router->generate("r_tapp_homepage");
+            return $this->redirect($url);
+        }
+
+
+            return $this->render('IUTUserBundle::signin.html.twig', array(
+                'form' => $form->createView()
+            ));
+
+    }
+
+
+
 }
